5a77a133e6b91652cd063223804c5261b4a57cc9
rgulli <rgulli@users.noreply.github.com>
2020-04-21 21:22:59 -0400

    Initial project structure.

---
+++ zotero2wordcloud/zotero2wordclould.py
@@ -0,0 +1,10 @@
+def zotero2wordcloud(words)
+    """
+    Create a histogram of words from some field of your Zotero collection.
+    Args: 
+        words (str): words from the text
+    Returns: 
+        None
+    """
+
+    print(f"Here are the words from the Zotero: {words}.")
