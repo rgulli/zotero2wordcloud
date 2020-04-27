c2f484ec37885432f939adb2acc2495dcd5b7453
rgulli <rgulli@users.noreply.github.com>
2020-04-27 08:41:11 -0400

    Update setup.py
    Added 'anytree'

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rgulli/zotero2wordcloud/",
     packages=find_packages(),
-    install_requires=['wordcloud', 'pyzotero'],
+    install_requires=['wordcloud', 'pyzotero','anytree'],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
